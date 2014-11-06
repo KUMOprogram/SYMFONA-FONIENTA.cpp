d3a058e1d796ac21d3818740f5800cad341e2d76
Kumo <destroyer_raigeki@hotmail.com>
2014-11-06 20:30:10 +0200

    Create SYMFONA-FONIENTA.cpp

---
+++ SYMFONA-FONIENTA.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+    system("color f1");
+           string w,w1,w2;
+           int i,sym=0,fon=0;
+           
+           cout<<" Dwse le3h : ";
+           cin>>w;
+
+		   for (i=0; i<w.size(); ++i)
+		   {
+				if (w[i]=='a' || w[i]=='e' || w[i]=='i'  || w[i]=='o'  || w[i]=='u'  || w[i]=='w'  || w[i]=='y'  || w[i]=='A'  || w[i]=='E'  || w[i]=='I'  || w[i]=='O' || w[i]=='U' || w[i]=='W' || w[i]=='Y')
+				{
+					++fon;
+				}else{
+					++sym;
+				}
+		   }
+		   
+		   for (i=0; i<w.size(); ++i)
+		   {
+		   		if (w[i]=='a' || w[i]=='e' || w[i]=='i'  || w[i]=='o'  || w[i]=='u'  || w[i]=='w'  || w[i]=='y'  || w[i]=='A'  || w[i]=='E'  || w[i]=='I'  || w[i]=='O' || w[i]=='U' || w[i]=='W' || w[i]=='Y')
+		   		{
+						w1+=w[i];
+		   		}else{
+                		w2+=w[i];
+		   		}
+			}
+		   
+           cout<<endl<<" SYMFWNA: "<<sym<<" ---> "<<w2<<endl<<" FWNHENTA: "<<fon<<" ---> "<<w1<<endl<<endl;
+
+    system("pause");
+}
